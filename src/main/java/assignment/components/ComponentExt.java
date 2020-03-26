8b7971e16af5f5e2e69b91535fc5d74397846101
EtK2000 <eytannovick@gmail.com>
2020-03-26 23:53:27 +0200

    Added ComponentExt class
    Added ComponentExt class, to allow adding instanced helper functions into components.

---
+++ src/main/java/assignment/components/ComponentExt.java
@@ -0,0 +1,15 @@
+package assignment.components;
+
+import net.gameslabs.api.Component;
+import net.gameslabs.api.Player;
+import net.gameslabs.events.GetPlayerLevel;
+import net.gameslabs.model.Skill;
+
+/** A drop-in replacement for {@link Component} with helper functions */
+abstract class ComponentExt extends Component {
+    final protected int getPlayerLevel(Player player, Skill skill) {
+        GetPlayerLevel getPlayerLevel = new GetPlayerLevel(player, skill);
+        send(getPlayerLevel);
+        return getPlayerLevel.getLevel();
+    }
+}
--- src/main/java/assignment/components/MyMineComponent.java
+++ src/main/java/assignment/components/MyMineComponent.java
@@ -3,12 +3,10 @@ package assignment.components;
 import assignment.events.InventoryAddEvent;
 import assignment.events.OreMineEvent;
 import assignment.model.Item;
-import net.gameslabs.api.Component;
-import net.gameslabs.events.GetPlayerLevel;
 import net.gameslabs.events.GiveXpEvent;
 import net.gameslabs.model.Skill;
 
-public class MyMineComponent extends Component {
+public class MyMineComponent extends ComponentExt {
     @Override
     public void onLoad() {
         registerEvent(OreMineEvent.class, this::onMine);
@@ -16,19 +14,16 @@ public class MyMineComponent extends Component {
 
     // add the item to the player's inventory
     private void onMine(OreMineEvent event) {
-        GetPlayerLevel getPlayerLevel = new GetPlayerLevel(event.getPlayer(), Skill.MINING);
-        send(getPlayerLevel);
-        
-        if (getPlayerLevel.getLevel() < event.getOre().getMinMineLvl())
-            event.setCancelled(true);// you cannot mine X with low lvl
-        
+        if (getPlayerLevel(event.getPlayer(), Skill.MINING) < event.getOre().getMinMineLvl())
+            event.setCancelled(true);// you cannot mine X with your lvl
+
         else {
             // intern concatenation because it will be used often if multiple ores exist
             InventoryAddEvent add = new InventoryAddEvent(event.getPlayer(), new Item((event.getOre().name() + " Ore").intern()));
             send(add);
             if (add.getItem() != null)
                 event.setCancelled(true);
-            
+
             // only mine it if we have inventory space to add it
             else
                 send(new GiveXpEvent(event.getPlayer(), Skill.MINING, event.getOre().getMineXP()));
